83818357b8cb1581740c2c24dab6b05680eb70d2
Arnav Payghan <112773141+arnav-payghan@users.noreply.github.com>
2025-08-06 00:15:23 +0530

    New frontend.

--- nexo-chat/src/pages/HomePage.jsx
+++ nexo-chat/src/pages/HomePage.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Sidebar from '../components/Sidebar.jsx'
+import ChatContainer from '../components/ChatContainer.jsx'
+import RightSidebar from '../components/RightSidebar.jsx'
 
 const HomePage = () => {
+
+  const  [ selectedUser, setSelectedUser ] = useState(false);
+
   return (
     <div className="border w-full h-screen sm:px-[15%] sm:py-[5%] ">
-      <h1>Home Page</h1>
+      <div className={`grid grid-cols-1 backdrop-blur-xl border-2 border-gray-700 rounded-2xl overflow-hidden 
+      h-[100%] relative ${ selectedUser ? "md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]" : "md:grid-cols-2" } `}>
+        <Sidebar selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
+        <ChatContainer selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
+        <RightSidebar selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
+      </div>
     </div>
   )
 }
